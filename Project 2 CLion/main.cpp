ad19354d933cb5bb179bb15bfb555fc54d4308ea
Robert Kramer <rwkramer@ysu.edu>
2023-09-29 10:56:21 -0400

    added initial Programming folder and project 2 skeletons

---
+++ Project 2 CLion/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <fraction.h>
+
+using namespace std;
+
+int main() {
+    Fraction
+        f;
+
+    cin >> f;
+
+    f = f + 1;
+
+    cout << f << endl;
+
+    return 0;
+}
